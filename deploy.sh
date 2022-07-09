8c02cdf999ef2519dca7c40643bcb9cef643f193
huangbin <1127779503@qq.com>
2022-07-09 23:56:19 +0800

    .............

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ cd docs/.vuepress/dist
 
 msg='来自github actions的自动部署'
 githubUrl=https://buzhifanji:${GITHUB_TOKEN}@github.com/Buzhifanji/leetcode-book.git
-git config --global user.name "buzhifanji"
-git config --global user.email "buzhifanji@163.com"
-git config --global init.defaultBranch "main"
+# git config --global user.name "buzhifanji"
+# git config --global user.email "buzhifanji@163.com"
+# git config --global init.defaultBranch "main"
 
 git init
 git add -A
