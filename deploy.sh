511aba1e08eedb0baf8cb823f4b8976a245bd572
huangbin <1127779503@qq.com>
2022-07-09 23:29:41 +0800

    修改自动部署

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,19 @@ npm run build
 # 进入生成的文件夹
 cd docs/.vuepress/dist
 
-git config --global user.name "buzhifanji"
-git config --global user.email "buzhifanji@163.com"
-
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='deploy'
+  githubUrl=git@github.com:Buzhifanji/blog.git
+else
+  msg='来自github actions的自动部署'
+  githubUrl=https://buzhifanji:${GITHUB_TOKEN}@github.com/Buzhifanji/blog.git
+  git config --global user.name "buzhifanji"
+  git config --global user.email "buzhifanji@163.com"
+  git config --global init.defaultBranch "main"
+fi
 git init
-git checkout -b main
 git add -A
-git commit -m "来自github actions的自动部署"
-git push -f https://buzhifanji:${GITHUB_TOKEN}@github.com/Buzhifanji/leetcode-book.git main:gh-pages # 推送到github gh-pages分支
-
+git commit -m "${msg}"
+git push -f $githubUrl main:gh-pages # 推送到github gh-pages分支
 cd -
 rm -rf docs/.vuepress/dist
