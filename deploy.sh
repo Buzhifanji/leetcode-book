27d4259c1a95583b7e0c0bc517de42ad1ddfedd5
huangbin <1127779503@qq.com>
2022-07-09 23:30:55 +0800

    ..........

--- deploy.sh
+++ deploy.sh
@@ -11,10 +11,10 @@ cd docs/.vuepress/dist
 
 if [ -z "$GITHUB_TOKEN" ]; then
   msg='deploy'
-  githubUrl=git@github.com:Buzhifanji/blog.git
+  githubUrl=git@github.com:Buzhifanji/leetcode-book.git
 else
   msg='来自github actions的自动部署'
-  githubUrl=https://buzhifanji:${GITHUB_TOKEN}@github.com/Buzhifanji/blog.git
+  githubUrl=https://buzhifanji:${GITHUB_TOKEN}@github.com/Buzhifanji/leetcode-book.git
   git config --global user.name "buzhifanji"
   git config --global user.email "buzhifanji@163.com"
   git config --global init.defaultBranch "main"
