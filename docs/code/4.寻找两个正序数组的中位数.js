b059818a278901c3a79893ec5bfcc2bbc3401a1a
buzhifanji <buzhifanji@163.com>
2023-01-11 15:04:17 +0800

    寻找两个正序数组的中位数 暴力解法

--- docs/code/4.寻找两个正序数组的中位数.js
+++ docs/code/4.寻找两个正序数组的中位数.js
@@ -5,82 +5,108 @@
  * [4] 寻找两个正序数组的中位数
  */
 
+
 // @lc code=start
 /**
+ * 暴力解法
+ * 时间复杂度： 遍历全部数组 O(m+n)
+ * 空间复杂的： 开辟了一个数组，保存合并后的两个数组 O(m+n)
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number}
  */
 var findMedianSortedArrays = function (nums1, nums2) {
-  const totalLength = nums1.length + nums2.length;
-
-  // 先处理特殊的情况
-  if (totalLength === 0) {
-    return undefined
+  // 合并数组
+  const arr = [...nums1, ...nums2];
+  // 排序
+  arr.sort((a, b) => a - b)
+  const n = arr.length;
+  // 获取中位数
+  const mediumRankedIndex = Math.floor((n + 1) / 2 - 1);
+  const toFixed = val => +val.toFixed(5)
+  if (n % 2) {
+    // 奇数
+    return toFixed(arr[mediumRankedIndex])
+  } else {
+    // 偶数
+    return toFixed((arr[mediumRankedIndex] + arr[mediumRankedIndex + 1]) / 2)
   }
-
-  // 先假定 nums1 和 muns2 合并成一个数组，然后找到中位数的索引
-  const mediumRankedIndex = Math.floor((totalLength + 1) / 2 - 1);
-
-  // 如果是偶数，则需要获取两位数字，反之一位即可
-  const isOld = totalLength % 2 === 0 ? true : false;
-
-  let m = 0, // 记录 num1 查找的索引
-    n = 0,  // 记录 mun2 查找的索引
-    index = 0, // 记录假定合并数组 查找的索引
-    mediumRanked = 0, // 中位数
-    flag = false;
-
-  const findIndex = (val) => {
-    if (flag) {
-      mediumRanked = (mediumRanked + val) / 2;
-      return true
-    }
-
-    let result = false;
-
-    if (index === mediumRankedIndex) {
-      mediumRanked = val;
-      if (isOld) {
-        // 继续查找下一位
-        flag = true;
-      } else {
-        // 结束查找
-        result = true
-      }
-    }
-
-    index++
-    return result
-  }
-
-  while (m < nums1.length || n < nums2.length) {
-    const m1 = nums1[m];
-    const n1 = nums2[n];
-
-    if (m1 === undefined) {
-      if (findIndex(n1)) break;
-
-      n++
-    } else if (n1 === undefined) {
-      if (findIndex(m1)) break;
-
-      m++
-    } else {
-      if (m1 < n1) {
-        if (findIndex(m1)) break;
-
-        m++;
-      } else {
-        if (findIndex(n1)) break;
-
-        n++;
-      }
-    }
-  }
-
-  return +mediumRanked.toFixed(5)
 };
+// /**
+//  * @param {number[]} nums1
+//  * @param {number[]} nums2
+//  * @return {number}
+//  */
+// var findMedianSortedArrays = function (nums1, nums2) {
+//   const totalLength = nums1.length + nums2.length;
+
+//   // 先处理特殊的情况
+//   if (totalLength === 0) {
+//     return undefined
+//   }
+
+//   // 先假定 nums1 和 muns2 合并成一个数组，然后找到中位数的索引
+//   const mediumRankedIndex = Math.floor((totalLength + 1) / 2 - 1);
+
+//   // 如果是偶数，则需要获取两位数字，反之一位即可
+//   const isOld = totalLength % 2 === 0 ? true : false;
+
+//   let m = 0, // 记录 num1 查找的索引
+//     n = 0,  // 记录 mun2 查找的索引
+//     index = 0, // 记录假定合并数组 查找的索引
+//     mediumRanked = 0, // 中位数
+//     flag = false;
+
+//   const findIndex = (val) => {
+//     if (flag) {
+//       mediumRanked = (mediumRanked + val) / 2;
+//       return true
+//     }
+
+//     let result = false;
+
+//     if (index === mediumRankedIndex) {
+//       mediumRanked = val;
+//       if (isOld) {
+//         // 继续查找下一位
+//         flag = true;
+//       } else {
+//         // 结束查找
+//         result = true
+//       }
+//     }
+
+//     index++
+//     return result
+//   }
+
+//   while (m < nums1.length || n < nums2.length) {
+//     const m1 = nums1[m];
+//     const n1 = nums2[n];
+
+//     if (m1 === undefined) {
+//       if (findIndex(n1)) break;
+
+//       n++
+//     } else if (n1 === undefined) {
+//       if (findIndex(m1)) break;
+
+//       m++
+//     } else {
+//       if (m1 < n1) {
+//         if (findIndex(m1)) break;
+
+//         m++;
+//       } else {
+//         if (findIndex(n1)) break;
+
+//         n++;
+//       }
+//     }
+//   }
+
+//   return +mediumRanked.toFixed(5)
+// };
 // @lc code=end
 
 
